72efc1f18735bfd28bfc3f8b16de7f5f532a05c9
Hong Jeesoo <jeesoo.hong@gmail.com>
2019-04-12 16:21:40 +0900

    코드 리팩터

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-let score = 0
+let score = 0;
+let answer = 0;
 
-function randomColor() {
-  const r = Math.floor(Math.random() * 256)
-  const g = Math.floor(Math.random() * 256)
-  const b = Math.floor(Math.random() * 256)
+const optionEls = document.querySelectorAll(".option");
+const colorTextEl = document.querySelector(".color-text");
+const nextStageBtn = document.querySelector(".next-stage");
+const playAgainBtn = document.querySelector(".play-again");
+const correctEl = document.querySelector(".correct");
+const wrongEl = document.querySelector(".wrong");
 
-  return `rgb(${r}, ${g}, ${b})`
-}
-
-document.querySelectorAll(".option").forEach((optionEl, index) => {
-  optionEl.addEventListener("click", e => {
+optionEls.forEach((optionEl, index) => {
+  optionEl.addEventListener("click", () => {
     if (answer === index) {
-      score++
-      document.querySelector('.correct').classList.add('open')
-     optionEl.classList.add("modal")
+      correctEl.classList.add("open");
+      optionEl.classList.add("modal");
+      score += 1;
     } else {
-      document.querySelector('.score-in-modal').textContent = score
-      score = 0
-      document.querySelector('.wrong').classList.add('open')
-        optionEl.classList.add("modal")
+      document.querySelector(".score-in-modal").textContent = score;
+      wrongEl.classList.add("open");
+      optionEl.classList.add("modal");
+      score = 0;
     }
-    document.querySelector('.score-num').textContent = score
-  })
-})
+    document.querySelector(".score-num").textContent = score;
+  });
+});
 
-function newStage() {
-  const options = [randomColor(), randomColor(), randomColor()]
-
-  document.querySelectorAll(".option").forEach((optionEl, index) => {
-    optionEl.style.backgroundColor = options[index]
-  })
-
-  answer = Math.floor(Math.random() * 3)
-
-  document.querySelector(".color-text").textContent = options[answer]
-}
-
-let answer
-
-newStage()
-
-document.querySelector('.next-stage').addEventListener('click', e => {
-  document.querySelector('.correct').classList.remove('open')
-  document.querySelector(".modal").classList.remove('modal')
-  newStage();
-})
-
-document.querySelector('.play-again').addEventListener('click', e => {
-  document.querySelector('.wrong').classList.remove('open')
+nextStageBtn.addEventListener("click", e => {
+  correctEl.classList.remove("open");
   document.querySelector(".modal").classList.remove("modal");
   newStage();
-})
+});
+
+playAgainBtn.addEventListener("click", e => {
+  wrongEl.classList.remove("open");
+  document.querySelector(".modal").classList.remove("modal");
+  newStage();
+});
+
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const newStage = (() => {
+  const options = [randomColor(), randomColor(), randomColor()];
+
+  optionEls.forEach((optionEl, index) => {
+    optionEl.style.backgroundColor = options[index];
+  });
+
+  answer = Math.floor(Math.random() * 3);
+
+  colorTextEl.textContent = options[answer];
+})();
